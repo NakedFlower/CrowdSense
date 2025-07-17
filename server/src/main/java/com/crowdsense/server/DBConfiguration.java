c25a0f1403370cd1bd0fca3ca51512acd1de43be
obbcth <obbcth@naver.com>
2025-07-17 21:16:24 +0900

    feat: support IAM role

--- server/src/main/java/com/crowdsense/server/DBConfiguration.java
+++ server/src/main/java/com/crowdsense/server/DBConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -17,22 +19,29 @@ public class DBConfiguration {
     private final Dotenv dotenv;
 
     public DBConfiguration() {
-        this.dotenv = Dotenv.load();
+        this.dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
     }
 
-    private String getAccessKey() {
-        return dotenv.get("AWS_ACCESS_KEY");
-    }
+    private AwsCredentialsProvider resolveCredentialsProvider() {
+        String accessKey = dotenv.get("AWS_ACCESS_KEY");
+        String secretKey = dotenv.get("AWS_SECRET_KEY");
 
-    private String getSecretKey() {
-        return dotenv.get("AWS_SECRET_KEY");
+        // Debug Credential
+        if (accessKey != null && !accessKey.isEmpty() && secretKey != null && !secretKey.isEmpty()) {
+            return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+        }
+
+        // Build Credential (EC2 IAM Role)
+        return DefaultCredentialsProvider.create();
     }
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
             .region(Region.AP_NORTHEAST_2)
-            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(this.getAccessKey(), this.getSecretKey())))
+            .credentialsProvider(resolveCredentialsProvider())
             .build();
     }
 
